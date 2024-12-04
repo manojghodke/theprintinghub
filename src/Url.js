7976f3a7b2b05501a115c34733842de56204c93f
manojghodke <105859330+manojghodke@users.noreply.github.com>
2024-12-04 13:45:31 +0530

    url update

--- src/Url.js
+++ src/Url.js
@@ -1,5 +1,5 @@
-// let _url = "https://theprintinghubbackend-5jfo.onrender.com";
-let _url = "http://localhost:4000";
+let _url = "https://theprintinghubbackend-5jfo.onrender.com";
+// let _url = "http://localhost:4000";
 
 export const BASE_URL = () => {
   return _url;
